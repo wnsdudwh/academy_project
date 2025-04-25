bf8943f0833fef490226432568ed2583b4397aff
DESKTOP-P8CO93B\kb <dhdhwnsdud@gmail.com>
2025-04-25 16:06:44 +0900

    Favorite, Footer, Product 섹션 레이아웃 공사.

--- src/sections/Favorite.js
+++ src/sections/Favorite.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import '../static/css/Favorite.css';
 
+import subImg1 from '../static/image/test/best/sub1.png';
+import subImg2 from '../static/image/test/best/sub2.png';
+import subImg3 from '../static/image/test/best/sub3.png';
+import subImg4 from '../static/image/test/best/sub4.png';
+import subImg5 from '../static/image/test/best/sub5.png';
+
 const Favorite = () => {
     return (
-        <div className="w-full bg-fuchsia-400">
-            <div className="max-w-[1440px] w-full h-[380px] mx-auto bg-fuchsia-100">
-            <div className="w-full mb-9 font-semibold text-3xl text-zinc-800">Editor’s Choice</div>
-                <div className=""></div>
+        <div className="w-full bg-[#3A5863]">
+            <div className="max-w-[1440px] w-full h-[380px] mx-auto bg-[#5A5863]">
+            <div className="w-full favorite-tit">
+                <div className="mb-9 font-semibold text-3xl text-white text-center py-8">Editor’s Choice</div>
+            </div>
+
+                <div className="w-full bg-blue-900 flex flex-row justify-between gap-6">
+                    <div className="basis-1/5 bg-primary-300 w-[208px] h-[208px]">
+                        <img src={subImg1} alt="FAVOLRITE ITEM 1~5" />
+                    </div>
+                    <div className="basis-1/5 bg-primary-300 w-[208px] h-[208px]">
+                        <img src={subImg2} alt="FAVOLRITE ITEM 1~5" />
+                    </div>
+                    <div className="basis-1/5 bg-primary-300 w-[208px] h-[208px]">
+                        <img src={subImg3} alt="FAVOLRITE ITEM 1~5" />
+                    </div>
+                    <div className="basis-1/5 bg-primary-300 w-[208px] h-[208px]">
+                        <img src={subImg4} alt="FAVOLRITE ITEM 1~5" />
+                    </div>
+                    <div className="basis-1/5 bg-primary-300 w-[208px] h-[208px]">
+                        <img src={subImg5} alt="FAVOLRITE ITEM 1~5" />
+                    </div>
+
+                </div>
             </div>
         </div>
     )
--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -3,10 +3,12 @@ import '../static/css/Footer.css';
 
 const Footer = () => {
     return (
-        <div className="max-w-[1280px] w-full h-[150px] mx-auto bg-[#222222] text">
-            <div className="">FOOTER</div>
-            <div className=""></div>
-        </div>
+        <footer className="w-full bg-[#343434]">
+            <div className="max-w-[1280px] w-full h-[150px] mx-auto bg-[#222222] text">
+                <div className="">FOOTER</div>
+                <div className=""></div>
+            </div>
+        </footer>
     )
 }
 
